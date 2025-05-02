0ced9c751b6bbfbf270e6737654a5d5c37487a18
Sofi-Lav <33ternovaya@gmail.com>
2025-05-02 22:04:56 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-// Swiper ініціалізація
-new Swiper(".swiper", {
+const toggleBtn = document.querySelector(".menu-toggle");
+const navMenu = document.getElementById("nav");
+
+toggleBtn.addEventListener("click", () => {
+  toggleBtn.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+const swiper = new Swiper(".swiper", {
   loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
